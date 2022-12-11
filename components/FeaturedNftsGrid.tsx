6a289b38e0e51941298ccde313b10be7b203216a
chibu <the1kidconnect@gmail.com>
2022-12-11 02:38:09 -0800

    update marketplace page copy

--- components/FeaturedNftsGrid.tsx
+++ components/FeaturedNftsGrid.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const FeaturedNftsGrid: NextPage<{}> = ({ children }) => {
   return (
     <div className="fmbs-gallery fmbs-page-content">
-      <h1 className="fmbs-gallery__header">Featured Artists</h1>
+      <h1 className="fmbs-gallery__header">future modern marketplace</h1>
       <div className="fmbs-gallery-grid">{children}</div>
       <Button href="javascript://" value="See all artists" />
     </div>
